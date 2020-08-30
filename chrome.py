5e1c3c3db3ea44912c856085bd1530d8cd782939
Ark Srivastava <arksri14@gmail.com>
2020-08-30 15:25:23 +0530

    First Commit

---
+++ chrome.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+from selenium import webdriver
+import selenium
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.alert import Alert 
+from pyvirtualdisplay import Display  
+import time
+from datetime import datetime
+import pytz
+import sys
+
+options = Options()
+options.add_argument("--start-maximized")
+options.add_argument('use-fake-device-for-media-stream')
+options.add_argument("--no-sandbox") 
+options.add_argument("--disable-setuid-sandbox") 
+options.add_argument("--disable-dev-shm-using") 
+options.add_argument("--disable-extensions") 
+options.add_argument("--disable-gpu")
+options.add_argument("disable-infobars")
+
+driver = webdriver.Chrome(options=options)
+# tz_INDIA = pytz.timezone('Asia/Kolkata')
+# currTime = datetime.now(tz_INDIA).strftime("%H:%M")
+# given_time = "9:30"
+# while currTime != given_time:
+#     currTime = datetime.now(tz_INDIA).strftime("%H:%M")
+#     time.sleep
+# if currTime == given_time:
+Subject = sys.argv[1]
+Roll = sys.argv[2]
+Pass = sys.argv[3]
+driver.get("https://teams.microsoft.com/_?culture=en-in&country=IN&lm=deeplink&lmsrc=homePageWeb&cmpid=WebSignIn#/school/conversations/General?threadId=19:e55d2df493ee4cb296c880eba5a0675b@thread.tacv2&ctx=channel")
+def check_exists_by_id(id):
+    try:
+        driver.find_element_by_id(id)
+    except NoSuchElementException:
+        return False
+    return True
+login_email = driver.find_element_by_name("loginfmt")
+login_email.send_keys(Roll+"@nitt.edu")
+# time.sleep(2)
+login_email_submit = driver.find_element_by_id("idSIButton9")
+login_email_submit.click()
+login_pass = driver.find_element_by_name("passwd")
+login_pass.send_keys(Pass)
+# time.sleep(2)
+try:
+    login_pass_submit = WebDriverWait(driver,600).until(EC.element_to_be_clickable((By.XPATH,"//*[@value=\"Sign in\"]")))
+    login_pass_submit.click()
+    stay_button_no = WebDriverWait(driver,60).until(EC.element_to_be_clickable((By.ID,"idBtn_Back")))
+    stay_button_no.click()
+    # time.sleep(10)
+    try:
+        card = WebDriverWait(driver,100).until(EC.presence_of_element_located((By.CLASS_NAME,"team-card")))
+        if len(driver.find_elements_by_class_name("team-name-text"))>0:
+            count = 0
+            for subject in driver.find_elements_by_class_name("team-name-text"):
+                if subject.text == Subject :
+                    subject.click()
+                    break
+                count = count + 1
+            if count == len(driver.find_elements_by_class_name("team-name-text")):
+                driver.execute_script("alert(\"Invalid Subject\");")
+                sys.exit(0)
+        try:
+            join_call = WebDriverWait(driver,1800).until(EC.element_to_be_clickable((By.XPATH,"//*[@id=\"m1598504362487\"]/calling-join-button/button")))
+            join_call.click()
+        except WebDriverException or NoSuchElementException:
+            try:
+                driver.execute_script("alert(\"Class hasn't started yet!\");")
+                sys.exit(0)
+            except WebDriverException:
+                pass
+        # time.sleep(5)
+            try:
+                no_video = WebDriverWait(driver,100).until(EC.element_to_be_clickable((By.XPATH,"//*[@id=\"page-content-wrapper\"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[2]/toggle-button[1]/div/button/span[1]")))
+                no_video.click()
+                no_audio = WebDriverWait(driver,100).until(EC.element_to_be_clickable((By.XPATH,"//*[@id=\"preJoinAudioButton\"]/div/button/span[1]")))
+                no_audio.click()
+                join_now = WebDriverWait(driver,100).until(EC.element_to_be_clickable((By.XPATH,"//*[@id=\"page-content-wrapper\"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[1]/div/div/button")))
+                join_now.click()
+            except WebDriverException or NoSuchElementException:
+                try:
+                    driver.execute_script("alert(\"Sorry, not able to join the call\");")
+                    sys.exit(0)
+                except WebDriverException:
+                    pass
+    except WebDriverException or NoSuchElementException:
+        try:
+            driver.execute_script("alert(\"Specified card not present at the moment\");")
+        except WebDriverException:
+            pass
+        # time.sleep(2)
+        # driver.switch_to.alert.accept()
+        # driver.quit()
+        # time.sleep(5)
+except WebDriverException or NoSuchElementException:
+    try:
+        driver.execute_script("alert(\"Element didn't load in the stipulated time\");")
+    except WebDriverException:
+        pass
+    time.sleep(2)
+    driver.close()
